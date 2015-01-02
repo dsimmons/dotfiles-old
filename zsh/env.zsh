d6f689e891b2fe07ecc0a817a64555abe53aa293
Dan Simmons <dan@simmons.io>
2015-01-02 12:36:18 -0500

    Unset $GOROOT because it appears to no longer be necessary (and in fact breaks things) in Go 1.4.

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -1,12 +1,5 @@
-current_path=$PATH
-
 # "Go" ahead (hahaha) and set $GOPATH regardless.
 export GOPATH=$HOME/.go
 
-# If golang binary exists, set $GOROOT.
-if command -v go >/dev/null 2>&1 ; then
-  export GOROOT=`go env GOROOT`
-  current_path=$current_path:$GOROOT/bin
-fi
-
-export PATH=$current_path:$GOPATH/bin
+# Add $GOPATH/bin to our PATH so that binaries installed with go are available.
+export PATH=$PATH:$GOPATH/bin
