4124097b7a1ce40148c127177224f242329f68c2
Dan Simmons <dan@simmons.io>
2015-09-07 12:19:21 -0400

    Added env vars for Google Cloud utilities.

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -3,3 +3,9 @@ export GOPATH=$HOME/.go
 
 # Add $GOPATH/bin to our PATH so that binaries installed with go are available.
 export PATH=$PATH:$GOPATH/bin
+
+# The next line updates PATH for the Google Cloud SDK.
+source '/Users/dsimmons/google-cloud-sdk/path.zsh.inc'
+
+# The next line enables zsh completion for gcloud.
+source '/Users/dsimmons/google-cloud-sdk/completion.zsh.inc'
